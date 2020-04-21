897ad84094117e20a978e89d4b25cde042797504
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-04-21 13:30:26 +0100

    configuring ESLint

---
+++ test/index.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+import {describe} from "mocha";
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
